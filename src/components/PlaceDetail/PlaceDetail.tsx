6c47c4ef744ec4c17b3fb83c8477268cc19a6553
Arden de Raaij <a.de.raaij@gmail.com>
2019-01-13 15:07:04 +0000

    [add] ionicons

--- src/components/PlaceDetail/PlaceDetail.tsx
+++ src/components/PlaceDetail/PlaceDetail.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Modal, View, Text, Image, Button } from 'react-native';
+import { Modal, View, Text, Image, Button, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const ModalWrap = styled.Modal`
   margin: 22px;
@@ -48,7 +49,10 @@ const PlaceDetail: React.SFC<Props> = ({ selectedPlace, onItemDeleted, onModalCl
     >
       <View>{modalContent}</View>
       <ButtonWrap>
-        <Button title="Delete" color="red" onPress={onItemDeleted} />
+        <TouchableOpacity onPress={onItemDeleted}>
+          <Icon size={30} name="ios-trash" color="red" />
+        </TouchableOpacity>
+        {/* <Button title="Delete" color="red" onPress={onItemDeleted} /> */}
         <Button title="Close" onPress={onModalClosed} />
       </ButtonWrap>
     </ModalWrap>
