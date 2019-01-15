da3e7a9fda14317ccd5b1bad152ee4a8dd09c190
Arden de Raaij <a.de.raaij@gmail.com>
2019-01-15 13:51:50 +0000

    [add] tab navigation and icons

--- src/screens/MainTabs/startMainTabs.tsx
+++ src/screens/MainTabs/startMainTabs.tsx
@@ -1,51 +1,71 @@
 import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const myTabNavigation = () =>
+const myTabNavigation = async () => {
+  const icons = await Promise.all([
+    Icon.getImageSource('md-map', 30),
+    Icon.getImageSource('md-share-alt', 30)
+  ]);
   Navigation.setRoot({
     root: {
       bottomTabs: {
         id: 'BottomTabsId',
-        options: {
-          topBar: {
-            visible: true
-          }
-        },
         children: [
           {
-            component: {
-              id: 'sharescreen',
-              name: 'awesome-places.SharePlaceScreen',
-              options: {
-                topBar: {
-                  visible: true,
-                  title: {
-                    text: 'Share Place'
+            stack: {
+              id: 'Tabs',
+              children: [
+                {
+                  component: {
+                    id: 'sharescreen',
+                    name: 'awesome-places.SharePlaceScreen',
+                    options: {
+                      topBar: {
+                        visible: true,
+                        title: {
+                          text: 'Share Place'
+                        }
+                      },
+                      bottomTab: {
+                        fontSize: 12,
+                        text: 'Share Place',
+                        icon: icons[0]
+                      }
+                    }
                   }
-                },
-                bottomTab: {
-                  fontSize: 12,
-                  text: 'Share Place',
-                  icon: require('../../assets/signin.png')
                 }
-              }
+              ]
             }
           },
           {
-            component: {
-              id: 'findplace',
-              name: 'awesome-places.FindPlaceScreen',
-              options: {
-                bottomTab: {
-                  text: 'Find Place',
-                  fontSize: 12,
-                  icon: require('../../assets/signup.png')
+            stack: {
+              id: 'Tabs2',
+              children: [
+                {
+                  component: {
+                    id: 'findscreen',
+                    name: 'awesome-places.FindPlaceScreen',
+                    options: {
+                      topBar: {
+                        visible: true,
+                        title: {
+                          text: 'Find Place'
+                        }
+                      },
+                      bottomTab: {
+                        fontSize: 12,
+                        text: 'Find Place',
+                        icon: icons[1]
+                      }
+                    }
+                  }
                 }
-              }
+              ]
             }
           }
         ]
       }
     }
   });
-
+};
 export default myTabNavigation;
