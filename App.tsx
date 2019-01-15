9b6e2295153cc78cb6b0bb70c60f2278e11ad735
Arden de Raaij <a.de.raaij@gmail.com>
2019-01-15 18:41:13 +0000

    [add] createPage component for RNN + Redux and  clean file

--- App.tsx
+++ App.tsx
@@ -1,18 +1,44 @@
+import * as React from 'react';
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
 
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
+import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
+
+const createPage = (Component, ...props) => {
+  return class App extends React.Component {
+    render() {
+      return (
+        <Provider store={store}>
+          <Component
+            {...{
+              ...this.props,
+              ...props
+            }}
+          />
+        </Provider>
+      );
+    }
+  };
+};
 // Register Screens
-Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen);
-Navigation.registerComponent('awesome-places.SharePlaceScreen', () => SharePlaceScreen);
-Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlaceScreen);
+Navigation.registerComponent('awesome-places.AuthScreen', () => createPage(AuthScreen));
+Navigation.registerComponent('awesome-places.SharePlaceScreen', () => createPage(SharePlaceScreen));
+Navigation.registerComponent('awesome-places.FindPlaceScreen', () => createPage(FindPlaceScreen));
+Navigation.registerComponent('awesome-places.PlaceDetailScreen', () =>
+  createPage(PlaceDetailScreen)
+);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
       stack: {
-        id: 'TEST',
+        id: 'Auth',
         children: [
           {
             component: {
@@ -32,99 +58,3 @@ Navigation.events().registerAppLaunchedListener(() => {
     }
   });
 });
-
-// import * as React from 'react';
-// import styled from 'styled-components/native';
-// import { connect } from 'react-redux';
-
-// import PlaceInput from './src/components/PlaceInput/PlaceInput';
-// import PlaceList from './src/components/PlaceList/PlaceList';
-// import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
-// import { addPlace, deletePlace, selectPlace, deselectPlace } from './src/store/actions';
-
-// const AppContainer = styled.View`
-//   display: flex;
-//   flex: 1;
-//   padding: 30px 10px 0 10px;
-//   justify-content: flex-start;
-//   align-items: center;
-//   background-color: #fff;
-// `;
-
-// interface State {
-//   places: Array<Place>;
-//   selectedPlace: Place;
-// }
-
-// const state = {
-//   places: [],
-//   selectedPlace: {}
-// };
-
-// interface Place {
-//   key: string;
-//   placeName: string;
-//   placeImage: {
-//     uri: string;
-//   };
-// }
-
-// // interface Props {}
-// class App extends React.Component<any> {
-//   placeAddedHandler = (placeName: string) => {
-//     console.log(placeName);
-//     this.props.onAddPlace(placeName);
-//   };
-
-//   placeSelectedHandler = (key: number) => {
-//     this.props.onSelectPlace(key);
-//   };
-//   placeDeletedHandler = (key: number) => {
-//     this.props.onDeletePlace(key);
-//   };
-//   modalClosedHandler = () => {
-//     this.props.onDeselectPlace();
-//   };
-//   public render() {
-//     return (
-//       <AppContainer>
-//         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-//         <PlaceList onItemSelected={this.placeSelectedHandler} places={this.props.places} />
-//         <PlaceDetail
-//           selectedPlace={this.props.selectedPlace}
-//           onItemDeleted={this.placeDeletedHandler}
-//           onModalClosed={this.modalClosedHandler}
-//         />
-//       </AppContainer>
-//     );
-//   }
-// }
-
-// const mapStateToProps = (state: State) => ({
-//   places: state.places.places,
-//   selectedPlace: state.places.selectedPlace
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   onAddPlace: (name: string) => dispatch(addPlace(name)),
-//   onDeletePlace: () => dispatch(deletePlace()),
-//   onSelectPlace: (key: number) => dispatch(selectPlace(key)),
-//   onDeselectPlace: () => dispatch(deselectPlace())
-// });
-
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(App);
-
-// import React, { Component } from 'react'
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View>
-//         <Text>Text</Text>
-//       </View>
-//     );
-//   }
-// }
