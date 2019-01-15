fd888d9e045ba14d6ada10869ca0f25fb4988ed2
Arden de Raaij <a.de.raaij@gmail.com>
2019-01-15 18:44:12 +0000

    [create] FindPlaceScreen

--- src/screens/FindPlace/FindPlace.tsx
+++ src/screens/FindPlace/FindPlace.tsx
@@ -1,12 +1,39 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { Navigation } from 'react-native-navigation';
+import PlaceList from '../../components/PlaceList/PlaceList';
 
-export default class FindPlaceScreen extends React.Component {
+class FindPlaceScreen extends React.Component {
+  itemSelectedHandler = key => {
+    Navigation.push(this.props.componentId, {
+      component: {
+        name: 'awesome-places.PlaceDetailScreen',
+        passProps: {
+          selectedPlace: this.props.places.find(item => item.key === key)
+        },
+        options: {
+          topBar: {
+            title: {
+              text: this.props.places.find(item => item.key === key).name
+            }
+          }
+        }
+      }
+    });
+  };
   render() {
     return (
       <View>
-        <Text>Find Place Screen</Text>
+        <PlaceList places={this.props.places} onItemSelected={this.itemSelectedHandler} />
       </View>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    places: state.places.places
+  };
+};
+export default connect(mapStateToProps)(FindPlaceScreen);
