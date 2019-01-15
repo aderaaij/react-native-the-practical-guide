dfc7ddcdf7825f0acb103b2109cfb3ca88a230c0
Arden de Raaij <a.de.raaij@gmail.com>
2019-01-15 18:43:11 +0000

    [delete] select/deselect reducers / actions

--- src/store/actions/places.tsx
+++ src/store/actions/places.tsx
@@ -1,19 +1,11 @@
-import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from './actionTypes';
+import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName: string) => ({
   type: ADD_PLACE,
   placeName
 });
 
-export const deletePlace = () => ({
-  type: DELETE_PLACE
-});
-
-export const selectPlace = (key: number) => ({
-  type: SELECT_PLACE,
+export const deletePlace = (key: number) => ({
+  type: DELETE_PLACE,
   placeKey: key
-});
-
-export const deselectPlace = () => ({
-  type: DESELECT_PLACE
 });
