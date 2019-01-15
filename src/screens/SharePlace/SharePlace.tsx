e9a05322b342831ba29b8c0acac8a09a55a6cb30
Arden de Raaij <a.de.raaij@gmail.com>
2019-01-15 18:45:07 +0000

    [create] SharePlaceScreen

--- src/screens/SharePlace/SharePlace.tsx
+++ src/screens/SharePlace/SharePlace.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { addPlace } from '../../store/actions/index';
 
-export default class SharePlaceScreen extends React.Component {
+import PlaceInput from '../../components/PlaceInput/PlaceInput';
+
+class SharePlaceScreen extends React.Component {
+  placeAddedHandler = (placeName: string) => {
+    this.props.onAddPlace(placeName);
+  };
   render() {
     return (
       <View>
-        <Text>Share Place Screen</Text>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
       </View>
     );
   }
 }
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace: (placeName: string) => dispatch(addPlace(placeName))
+  };
+};
+export default connect(
+  null,
+  mapDispatchToProps
+)(SharePlaceScreen);
