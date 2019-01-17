e7bdac4c96117181313457713f522faa961dfabf
Arden de Raaij <a.de.raaij@gmail.com>
2019-01-17 17:14:15 +0000

    [fix] types

--- src/screens/PlaceDetail/PlaceDetail.tsx
+++ src/screens/PlaceDetail/PlaceDetail.tsx
@@ -1,39 +1,42 @@
 import * as React from 'React';
-import { View, Button, TouchableOpacity } from 'react-native';
-import styled from 'styled-components/native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 import { Navigation } from 'react-native-navigation';
+
 import { deletePlace } from '../../store/actions/index';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const ModalWrap = styled.Modal`
-  margin: 22px;
-`;
-
-const TitleText = styled.Text`
+const TitleText = styled(Text)`
   font-size: 28px;
   font-weight: 700;
   text-align: center;
 `;
 
-const ButtonWrap = styled.View`
+const ButtonWrap = styled(View)`
   display: flex;
   flex-direction: row;
 `;
 
-const FeaturedImage = styled.Image`
+const FeaturedImage = styled(Image)`
   width: 100%;
   height: 200px;
 `;
 
-interface Props {
+export interface PlaceDetailProps {
+  onDeletePlace: (key: number) => void;
   selectedPlace: {
+    key: number;
     name: string;
-    image: object;
+    image: {
+      uri: string;
+    };
   };
+  componentId: string;
 }
 
-class PlaceDetail extends React.Component {
+class PlaceDetail extends React.Component<PlaceDetailProps> {
   placeDeletedHandler = () => {
     this.props.onDeletePlace(this.props.selectedPlace.key);
     Navigation.pop(this.props.componentId);
@@ -55,11 +58,12 @@ class PlaceDetail extends React.Component {
   }
 }
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch: Dispatch) => {
   return {
     onDeletePlace: (key: number) => dispatch(deletePlace(key))
   };
 };
+
 export default connect(
   null,
   mapDispatchToProps
